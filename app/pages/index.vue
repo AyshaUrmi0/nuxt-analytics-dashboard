bf8608b46224af22bbf8c646e27b28babc309d58
Ayhsa Urmi <ayshaismail021@gmail.com>
2026-08-11 01:06:34 +0600

    feat: create dashboard page with KPI cards and chart components

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,7 +16,7 @@
           </p>
         </div>
 
-        <!-- <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
 
           <KpiCard
             title="Total Revenue"
@@ -42,7 +42,7 @@
             change="+10.1%"
           />
 
-        </div> -->
+        </div>
 
         <div class="mt-6">
  <ChartsRevenueBarChart />
